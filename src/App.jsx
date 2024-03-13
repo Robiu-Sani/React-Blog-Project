e7d9007e65e184b8bf31582ad2d91962337a1bc2
*Robius-Sani* <*hafazrobiussani@gmail.com*>
2024-03-13 00:06:01 -0700

    adding data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Blogs from "./components/blogs/Blogs";
+import Header from "./components/header/Header";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header></Header>
+      <Blogs></Blogs>
+    </div>
   )
 }
---
+++ src/components/blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function Blogs() {
+    const [blogs , setBlogs] = useState([]);
+
+    useEffect( () => {
+        fetch('data.json')
+            .then(res => res.json()) 
+            .then(data => {
+                console.log(data);
+            })
+    }, []);
+
+    return (
+        <div>
+
+        </div>
+    );
+}
